a84ea2c167e955a8e3e14c75d71f999fb5c6460d
kobi-hershtiktec <kobi@hershtiktec.com>
2026-06-16 19:39:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// הגדרת ה-Base מבטיחה שהאתר לא יעלה כמסך לבן ב-GitHub Pages
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/Aura/', 
+  base: './', // משתמש בנתיב יחסי נוכחי, פותר בעיות 404 ב-GitHub Pages
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
